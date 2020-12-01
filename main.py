63d7f1f226bb179d01044218fc434db991a40161
Ayleen Roque <71857829+ayleenR@users.noreply.github.com>
2020-11-30 21:34:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#def calculate_study_hours(credit_hours):
+ #   study_hours = credit_hours ** 2
+  #  return study_hours
+
+#decision = int(input("Put here: "))
+#if (decision == 303):
+ #   print("To find how long to study, input the credit hours of your class")
+  #  credit_hours = int(input("Put credit hours here: "))
+   # print(calculate_study_hours(credit_hours))
+honey = int(input("Put here: "))
+year = int(input("Year number: "))
+not year == honey
+print("nice")
